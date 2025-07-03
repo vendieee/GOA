2a1e0e25678af05dcf2479f39da2c64cc7dfaafc
vendieee <lomiashviliandria4@gmail.com>
2025-07-03 16:09:24 +0400

    commit

--- day 001/homework/lesson 2.py
+++ day 001/homework/lesson 2.py
@@ -1,6 +1,35 @@
-from turtle import *
+#პირველი
 
-name=("andria")
-age=("14")
+print("სტაპილოსფერი")
+print("კალათბურთი")
+print("ხინკალი")
 
-print(str(name) + "aris" + str(age) + "wlis")
+#მეორე
+
+film1 = " green book"
+film2 = " interstellar"
+film3 = " terminator 2"
+film4 = " Django Unchained"
+film5 = " Avengers: Infinity War"
+
+print(film1 + film2 + film3 + film4 + film5)
+
+#მესამე
+
+#კონკატინაცია აერთებს ორ ან მოცემულ რაოდენობის სტრინგებს
+
+
+#მეოთხე
+
+#integer int
+#string str
+#float 
+#boolean bool
+
+
+
+#მეხუთე 
+name="ანდრია"
+age=14
+
+print("მე მქვია " + name + " და მე ვარ " + str(age) + " წლის")
