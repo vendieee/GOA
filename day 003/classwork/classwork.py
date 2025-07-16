b89457a83967c273e2c81541a95e315537fe2ca4
vendieee <lomiashviliandria4@gmail.com>
2025-07-16 13:48:54 +0400

    commit

---
+++ day 003/classwork/classwork.py
@@ -0,0 +1,6 @@
+example=input("მᲣᲨᲐᲝᲑᲡ ᲗᲣ ᲐᲠᲐ ᲩᲔᲛᲘ ᲛᲐᲪᲘᲕᲐᲠᲘ?")
+print((example), "დიახ")
+inputexample = 23
+#update
+example = 444
+print(example)
---
+++ day 003/homework/homework.py
@@ -0,0 +1,9 @@
+name = input("Enter Your Name: ")
+print("Hello", name)
+
+#input შემავალი ინფორმაციაა, output შედეგია. მაგ: პულტს ავიღებ, დავაჭერ და ჩაირთვება ტელევიზორი.
+x2 = input("text will be x2: ")
+print( x2, x2)
+
+blabla = input("")
+print("გასაგებია")
