065a689cb5e74e5d723ffd4ae6ee8e50f91ae123
vendieee <lomiashviliandria4@gmail.com>
2025-09-05 20:23:37 +0400

    commit

---
+++ day 019/homework/python.py
@@ -0,0 +1,23 @@
+#first
+
+#list ცვლადისგან განსხვავებით ინახავს მრავალ მნიშვნელობას.
+
+#second
+
+for i in range(1, 101):
+    print(i)
+
+#third
+
+items = ["TV", "Chair", "Table", "Book", "Desk"]
+
+print(items[2])
+print(items[4])
+
+#fourth
+
+nums = [5, 72, 23, 67, 45, 19, 30]
+
+for n in nums:
+    if n > 20:
+        print(n)
