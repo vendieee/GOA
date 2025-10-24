b42d7762a5a0aad134d492dcee020d3a7a6937b1
vendieee <lomiashviliandria4@gmail.com>
2025-10-24 23:58:30 +0400

    commit

--- day 029/homework/python.py
+++ day 029/homework/python.py
@@ -1,34 +1,58 @@
+# 6) array ნიშნავს მონაცემთა სიას (list)
+# def გამოიყენება ფუნქციის შესაქმნელად Python-ში
+
+
 # 1) lion() 
 def lion():
-    arr = ["apple", "banana", "cherry", "date", "fig"]
-    print("Index of 'cherry':", arr.index("cherry"))
+    animals = ["dog", "cat", "lion", "tiger", "bear"]
+    print(animals.index("lion"))  
+
 
 # 2) lion2() 
 def lion2():
-    arr = [1,1,1,1,1,1,1,2]
-    last_element = arr[-1]
-    print("Index of last element:", arr.index(last_element))
+    nums = [1, 1, 1, 1, 1, 1, 1, 2]
+    last_index = nums.index(2)  
+    print(nums[last_index])     
+
 
 # 3) lion3() 
-def lion3():
-    name = input("შეიყვანე შენი სახელი: ")
-    print(name + " მე ვარ ლომი")  
+def lion3(name):
+    print(name, "მე ვარ ანანო ლომი <3")
 
 
 # 4) lion4() 
 def lion4():
-    arr = []
-    for i in range(1, 21):  
+    odd_numbers = []
+    for i in range(1, 11):
         if i % 2 != 0:
-            arr.append(i)
-    print("Odd numbers:", arr)
+            odd_numbers.append(i)
+    print(odd_numbers)
+
 
 # 5) lion5() 
 def lion5():
-    password = "1234"  
-    user_input = ""
-    while user_input != password:
-        user_input = input("შეიყვანე პაროლი: ")
-        if user_input != password:
-            print("პაროლი არასწორია")
-    print("გაუმარჯოს, პაროლი სწორია")
+    password = "lion123"
+    guess = ""
+    while guess != password:
+        guess = input("შეიყვანე პაროლი: ")
+        if guess != password:
+            print("პაროლი არასწორია!")
+    print("პაროლი სწორია!")
+
+
+# 7) 
+data = ["apple", "banana", "apple", "orange", "apple"]
+print(data.count("apple"))  # რამდენჯერაა "apple"
+
+
+# 8) 
+def multiply(a, b):
+    print(a * b)
+
+
+# 9) 
+def lion6():
+    animals = ["Lion", "Tiger", "Frog", "Panda", "Deer", "Elephant", "Fox"]
+    print(animals[1], animals[2])    
+    print(animals[-6], animals[-5])  
+
